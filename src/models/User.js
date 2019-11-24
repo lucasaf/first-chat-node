433848b968b812993a454d3636486fd1832c9df6
lucasfdte <lucas.alves@fdte.io>
2019-11-24 20:48:49 -0300

    create user model

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    nick_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
